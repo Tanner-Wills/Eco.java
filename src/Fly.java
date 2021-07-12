b9d8f7cd7c689f83731d2b089a5386e07532ee41
Tanner-Wills <tannerwills1@gmail.com>
2021-07-12 09:41:22 -0400

    Initial commit

---
+++ src/Fly.java
@@ -0,0 +1,23 @@
+
+
+public class Fly {
+
+    // ***** Fly Variables *****
+    public static float mass = 5;
+    public static double speed = 10;
+
+    // ***** Main Function *****
+    public static void main(String[] args) {
+
+
+    }
+
+
+    // ***** Fly Constructors *****
+
+
+
+
+
+
+}
