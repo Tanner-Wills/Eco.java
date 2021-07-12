36866b9ccb33db95d40955522895fd71ac2f8cb9
Tanner-Wills <tannerwills1@gmail.com>
2021-07-12 15:08:01 -0400

    Added getters and setters, changed mass / speed variables to private

--- src/Fly.java
+++ src/Fly.java
@@ -3,20 +3,50 @@
 public class Fly {
 
     // ***** Fly Variables *****
-    public static float mass = 5;
-    public static double speed = 10;
+    private double mass = 5;
+    private double speed = 10;
+
 
     // ***** Main Function *****
     public static void main(String[] args) {
 
+        Fly pepe = new Fly(4.1);
+        System.out.println(pepe);
 
     }
 
-
     // ***** Fly Constructors *****
+    public Fly(double mass, double speed){
+        this.mass = mass;
+        this.speed = speed;
+    }
+    public Fly(double mass){
+        this(mass, 10);
+    }
+    public Fly(){
+        this(5, 10);
+    }
 
 
+    // ***** Getters *****
+    public double getMass(){
+        return this.mass;
+    }
+    public double getSpeed(){
+        return this.speed;
+    }
 
+
+    // ***** Setters *****
+    public void setMass(double mass){
+        this.mass = mass;
+    }
+    public void setSpeed(double speed){
+        this.speed = speed;
+    }
+
+
+    // ***** Fly Methods *****
 
 
 
