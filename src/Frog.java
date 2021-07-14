0ef906212ad193e9077d712fdb8194863933f63f
Tanner-Wills <tannerwills1@gmail.com>
2021-07-14 15:51:17 -0400

    added Frog Grow method

--- src/Frog.java
+++ src/Frog.java
@@ -7,9 +7,9 @@ public class Frog {
     private String name;
     private int age = 5; // age in number of months
     private double tongueSpeed = 5;
-    private boolean isFroglet; // a Frog is a Froglet is its age > 1 month && age < months. This variable must be changed whenever age changes.
+    private boolean isFroglet; // a Frog is a Froglet is its age > 1 month && age < 7 months. This variable must be changed whenever age changes.
     private static String species = "Rare Pepe"; // must be the same for all instances of Frog
-    private double ageInYears = (double) (age/12);
+    private double ageInYears = (double) age/12;
 
     // ***** Frog Constructor *****
     public Frog(String name, int age, double tongueSpeed){
@@ -22,10 +22,10 @@ public class Frog {
         this("Luis", 5, 5);
     }
 
-    public Frog(double ageInYears){
-        this.ageInYears = (double) (age/12);
-        this("Bibi", ageInYears, 5);
-    }
+    //public Frog(double ageInYears){
+        //this.ageInYears = (double) age/12;
+        //this("Bibi",ageInYears, 5);
+    //}
 
     // ***** Getters *****
     public String getName(){
@@ -40,7 +40,9 @@ public class Frog {
     public boolean getIsFroglet(){
         return this.isFroglet;
     }
-
+    public double getAgeInYears() {
+        return ageInYears;
+    }
 
     // ***** Setters *****
     public void setName(String name){
@@ -55,5 +57,46 @@ public class Frog {
     public void setIsFroglet(boolean isFroglet){
         this.isFroglet = isFroglet;
     }
+    public void setAgeInYears(double ageInYears) {
+        this.ageInYears = ageInYears;
+    }
+
+    // ***** Methods *****
+    public int grow(int months){
+        if(age > 30){
+            tongueSpeed -= months;
+            if(tongueSpeed < 5){
+                tongueSpeed = 5;
+            }
+            isFroglet = false;
+            return age += months;
+        } else if (age > 12){
+            if(tongueSpeed < 5){
+                tongueSpeed = 5;
+            }
+            isFroglet = false;
+            return age += months;
+        } else {
+            if((age+months) > 12){
+                tongueSpeed += (12 - age);
+            } else {
+                tongueSpeed += months;
+            }
+            if(tongueSpeed < 5){
+                tongueSpeed = 5;
+            }
+            age += months;
+            if(age > 7){
+                isFroglet = false;
+            } else {
+                isFroglet = true;
+            }
+            return age;
+        }
+
+
+    }
+
+
 
 }
